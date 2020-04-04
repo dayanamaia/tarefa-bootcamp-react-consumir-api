701af2b759f4b3a13dd65f95a90a5f60b3ed94a3
Dayana Maia <dayrmaia@gmail.com>
2020-04-04 13:21:50 -0300

    estrutura dos componentes, custom hooks trazendo os filmes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/Studio_Ghibli_logo.png';
 import './App.css';
+
+import Movies from './Components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <Movies />
+      </main>
     </div>
   );
 }
