07769d0cf33d2b1fb469d9e64945b271b02f970f
Dayana Maia <dayrmaia@gmail.com>
2020-04-04 15:35:59 -0300

    Updates

---
+++ precache-manifest.03020172eb5db5c02a1664ac9d548ff1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc51d9ac5af6822bf925db340fa14ac3",
+    "url": "/tarefa-bootcamp-react-consumir-api/index.html"
+  },
+  {
+    "revision": "63a06fe48852e28b2466",
+    "url": "/tarefa-bootcamp-react-consumir-api/static/css/main.cf398970.chunk.css"
+  },
+  {
+    "revision": "ee267fdbe63d637b2faf",
+    "url": "/tarefa-bootcamp-react-consumir-api/static/js/2.a8e85166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tarefa-bootcamp-react-consumir-api/static/js/2.a8e85166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a06fe48852e28b2466",
+    "url": "/tarefa-bootcamp-react-consumir-api/static/js/main.31bce357.chunk.js"
+  },
+  {
+    "revision": "a693da108b71d76c9030",
+    "url": "/tarefa-bootcamp-react-consumir-api/static/js/runtime-main.9c60694f.js"
+  },
+  {
+    "revision": "a8f115a8a01dc0163afc55e1c8e42bc2",
+    "url": "/tarefa-bootcamp-react-consumir-api/static/media/Studio_Ghibli_logo.a8f115a8.png"
+  }
+]);
